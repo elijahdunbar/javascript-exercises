7142a98d6994fd4607237ae361d145e6b6aecacd
eli <eli.dunbar18@gmail.com>
2024-10-31 17:32:49 -0400

    completed exercises 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let strArray = Array.from(str);
+  let strReversed = '';
+  
+  for (let i = -1; i > -(strArray.length + 1); i--) {
+    strReversed += strArray.at(i);
+  }
+  return strReversed;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+   
+  for (item of itemsToRemove) {
+    while (array.indexOf(item) !== -1){
+    let index = array.indexOf(item);
+    array.splice(index, 1);
+    } 
+  }
+  return array;
 };
 
 // Do not edit below this line
