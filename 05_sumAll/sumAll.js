b36f0a3dbf2ed602bf859e4b84bfd1ea2a059f0e
eli <eli.dunbar18@gmail.com>
2024-10-31 19:39:24 -0400

    completed exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  let result = 0;
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    result = 'ERROR';
+  } else {
+    if (b > a) {
+        for (let i = a; i <= b; i++) {
+          result += i;
+        }
+    } else if (a > b) {
+      for (let i = b; i <= a; i++) {
+        result += i;
+      }
+    } 
+    
+  }
+  return result;
 };
+
+// console.log(Number.isInteger('99'));
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  const cTemp = Number(((fTemp - 32) * (5 / 9)).toFixed(1));
+  return cTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  const fTemp = Number((cTemp * (9/5) + 32).toFixed(1));
+  return fTemp;
 };
 
 // Do not edit below this line
